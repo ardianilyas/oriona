8553092137897f98b5e8e6874bfbb031b2856e42
ardianilyas <ardianilyas@gmail.com>
2025-04-16 13:10:06 +0700

    feat: add projects, tasks, comments, project_member migrations and models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Project;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +16,16 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user = User::factory()->create([
+            'name' => 'Ardian Ilyas',
+            'email' => 'ardian@oriona.com',
+            'password' => bcrypt('developer')
         ]);
+
+        $projects = Project::factory(10)->create([
+            'user_id' => $user->id,
+        ]);
+
+        User::factory(10)->has(Project::factory(10))->create();
     }
 }
